6f8db3b1638e471291a401e15647d09f265c7e93
Justin Forder <justin.forder@gmail.com>
2011-04-16 18:38:22 +0100

    Changed tabs to spaces.

--- models.js
+++ models.js
@@ -1,12 +1,12 @@
 function defineModels(mongoose) {
-	
-	var Document = new mongoose.Schema({
-		    title: {type: String, index: true},
-		    data: String,
-		    tags: [String]
-	    });
-
-	mongoose.model('Document', Document);	
+    
+    var Document = new mongoose.Schema({
+        title: {type: String, index: true},
+        data: String,
+        tags: [String]
+    });
+    
+    mongoose.model('Document', Document);
 }
 
 exports.defineModels = defineModels;
