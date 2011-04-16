189f80a59bbe63a144157490dac602f70b3f7392
Justin Forder <justin.forder@gmail.com>
2011-04-17 00:09:53 +0100

    Part 3

--- app.js
+++ app.js
@@ -1,49 +1,105 @@
+// Module dependencies.
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var mongoose = require('mongoose');
-var models = require('./models.js');
-
-models.defineModels(mongoose);
-
-var app = module.exports = express.createServer();
-
-mongoose.connect('mongodb://localhost/nodepad');
-var Document = mongoose.model('Document');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    models = require('./models.js'),
+    app = module.exports = express.createServer(),
+    Document;
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+
+    models.defineModels(mongoose);
+    app.mongoose = mongoose;
+    app.Document = Document = mongoose.model('Document');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function() {
+    app.use(express.logger({ format: ':method :uri' }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    mongoose.connect('mongodb://localhost/nodepad-development');
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function() {
+    app.use(express.logger());
+    app.use(express.errorHandler()); 
+    mongoose.connect('mongodb://localhost/nodepad-production');
+});
+
+app.configure('test', function() {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    mongoose.connect('mongodb://localhost/nodepad-test');
 });
 
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+// Document list
+app.get('/documents.:format', function(req, res) {
+    Document.find({}, function(err, documents) {
+        switch (req.params.format) {
+            case 'json':
+                res.send(documents.map(function(d) {
+                    return d.toObject();
+                }));
+                break;
+            default:
+                res.render('documents/index.jade');
+        }
+    });
+});
+
+// Create document 
+app.post('/documents.:format?', function(req, res) {
+    var document = new Document(req.body['document']);
+    document.save(function() {
+        switch (req.params.format) {
+            case 'json':
+                res.send(document.toObject());
+                break;
+                 
+            default:
+                res.redirect('/documents');
+        }
+    });
+});
+
+// Read document
+app.get('/documents/:id.:format?', function(req, res) {
+    res.render('index', {
+        title: 'Get ' + req.params.id
+    });
+});
+
+// Update document
+app.put('/documents/:id.:format?', function(req, res) {
+    res.render('index', {
+        title: 'Put ' + req.params.id
+    });
+});
+
+// Delete document
+app.del('/documents/:id.:format?', function(req, res) {
+    res.render('index', {
+        title: 'Delete ' + req.params.id
+    });
 });
 
 // Only listen on $ node app.js
 
 if (!module.parent) {
-  app.listen(3000);
-  console.log("Express server listening on port %d", app.address().port);
+    app.listen(3000);
+    console.log("Express server listening on port %d", app.address().port);
 }
--- test/app.test.js
+++ test/app.test.js
@@ -1,21 +1,77 @@
-
 // Run $ expresso
 
-/**
- * Module dependencies.
- */
+// Force test environment
+process.env.NODE_ENV = 'test';
 
-var app = require('../app')
-  , assert = require('assert');
+// Module dependencies.
 
+var app = require('../app'),
+    assert = require('assert'),
+    lastID = '';
 
 module.exports = {
-  'GET /': function(){
-    assert.response(app,
-      { url: '/' },
-      { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
-      function(res){
-        assert.includes(res.body, '<title>Express</title>');
-      });
-  }
+    
+    'GET /': function() {
+        console.log('>>>>> Test: GET /');
+        assert.response(app,
+                        { url: '/' },
+                        { status: 200, 
+                          headers: { 'Content-Type': 'text/html; charset=utf-8' }},
+                        function(res){
+                            assert.includes(res.body, '<title>Express</title>');
+                        });
+    },
+  
+    'GET /documents.json': function() {
+        console.log('>>>>> Test: GET /documents.json');
+        assert.response(app,
+                        { url: '/documents.json' },
+                        { status: 200, 
+                          headers: { 'Content-Type': 'application/json' }},
+                        function(res) {
+                            var documents = JSON.parse(res.body);
+                            assert.type(documents, 'object')
+                            console.log('Documents: ' + documents); // *****
+                            documents.forEach(function(d) {
+                                console.log('Document id: ' + d._id); // *****
+                                app.Document.findById(d._id, function(err, document) {
+                                    document.remove();
+                                })
+                            });
+                        });
+    },
+
+  
+    'POST /documents.json': function() {
+        console.log('>>>>> Test: POST /documents.json');
+        assert.response(app, 
+                        { url: '/documents.json',
+                          method: 'POST',
+                          data: JSON.stringify({ document: { title: 'Test' } }),
+                          headers: { 'Content-Type': 'application/json' }}, 
+                         { status: 200,
+                          headers: { 'Content-Type': 'application/json' }},
+                        function(res) {
+                            var document = JSON.parse(res.body);
+                            assert.equal('Test', document.title);
+                            lastID = document._id;
+                        });
+    },
+
+    'HTML POST /documents': function() {
+        console.log('>>>>> Test: HTML POST /documents');
+        assert.response(app, 
+                        { url: '/documents',
+                          method: 'POST',
+                          data: 'document[title]=test',
+                          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }},
+                        { status: 302 }
+                       );
+    }
 };
+
+// This is a quick hack to allow the tests to finish and exit.
+setTimeout(function() {
+    console.log("***** C L O S I N G *****");
+    app.mongoose.connection.close();
+}, 2000);
